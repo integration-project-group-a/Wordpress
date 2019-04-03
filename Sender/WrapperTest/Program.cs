230f4a7fc43e6f1fae294026aa458493d0956d52
monkeyman-zero <44073137+monkeyman-zero@users.noreply.github.com>
2019-04-04 00:17:51 +0200

    Sender+RestSharp

---
+++ Sender/WrapperTest/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using RestSharp;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace Sender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Requests requests = new Requests();
+
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+                using (var connection = factory.CreateConnection())
+                using (var channel = connection.CreateModel())
+                {
+                    channel.ExchangeDeclare(exchange: "logs", type: "fanout");
+
+                    var message = requests.GetUser(1);
+                    var body = Encoding.UTF8.GetBytes(message);
+                    channel.BasicPublish(exchange: "logs",
+                                         routingKey: "",
+                                         basicProperties: null,
+                                         body: body);
+                    Console.WriteLine(" [x] Sent {0}", message);
+                }
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+
+            }
+    }
+}
---
+++ Sender/WrapperTest/Requests.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RestSharp;
+using System.Threading.Tasks;
+using RestSharp.Authenticators;
+
+namespace Sender
+{
+    class Requests
+    {
+
+
+        public void GetUsers() {
+            var client = new RestClient("http://10.3.56.25/wp-json/wp/v2/");
+            client.Authenticator = new HttpBasicAuthenticator("Front-end", "Front-end");
+
+            var request = new RestRequest("users", Method.GET);
+
+            request.RequestFormat = DataFormat.Json;
+
+            var resposne = client.Execute(request);
+
+            var content = resposne.Content;
+
+            Console.WriteLine(content);
+
+        }
+
+        public String GetUser(int userID)
+        {
+            var client = new RestClient("http://10.3.56.25/wp-json/wp/v2/");
+            client.Authenticator = new HttpBasicAuthenticator("Front-end", "Front-end");
+
+            var request = new RestRequest("users/{id}", Method.GET);
+            request.AddUrlSegment("id", userID);
+
+            request.RequestFormat = DataFormat.Json;
+
+            var resposne = client.Execute(request);
+
+            return resposne.ToString();
+
+        }
+
+        public void AddUser(User user)
+        {
+            var client = new RestClient("http://10.3.56.25/wp-json/wp/v2/");
+            client.Authenticator = new HttpBasicAuthenticator("Front-end", "Front-end");
+            var request = new RestRequest("users", Method.POST);
+
+            request.AddJsonBody(user);
+
+            var response = client.Post(request);  
+
+
+        }
+
+        public void UpdateUser(int id, User user)
+        {
+            User update = user;
+
+            var client = new RestClient("http://10.3.56.25/wp-json/wp/v2/");
+            client.Authenticator = new HttpBasicAuthenticator("Front-end", "Front-end");
+            var request = new RestRequest("users/{id}", Method.POST);
+                request.AddUrlSegment("id", id);
+
+            request.AddJsonBody(user);
+
+            IRestResponse response = client.Execute(request);
+
+
+        }
+
+        public void DeleteUser(int id)
+        {
+            var client = new RestClient("http://10.3.56.25/wp-json/wp/v2/");
+            client.Authenticator = new HttpBasicAuthenticator("Front-end", "Front-end");
+            var request = new RestRequest("users/{id}?force=true", Method.DELETE);
+            request.AddUrlSegment("id", id);
+            string json = "{" + "\"reassign\": \"1\"" + "}";
+
+            request.AddJsonBody(json);
+
+            IRestResponse response = client.Execute(request);
+        }
+
+    }
+}
