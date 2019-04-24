d6da67c112e273e2921b9c2aab4977185b906fd1
monkeyman-zero <44073137+monkeyman-zero@users.noreply.github.com>
2019-04-25 01:16:03 +0200

    sender, wrapper voor costum path

--- Sender/WrapperTest/Program.cs
+++ Sender/WrapperTest/Program.cs
@@ -13,37 +13,41 @@ namespace Sender
     {
         static void Main(string[] args)
         {
-            Requests requests = new Requests();
+            //Requests requests = new Requests();
+            RequestVis visReq = new RequestVis();
 
-            var factory = new ConnectionFactory() { HostName = "10.3.56.27" };
-            factory.UserName = "manager";
-            factory.Password = "ehb";
-            using (var connection = factory.CreateConnection())
-            using (var channel = connection.CreateModel())
-            {
-                channel.ExchangeDeclare(exchange: "logs", type: "fanout");
+            //var factory = new ConnectionFactory() { HostName = "10.3.56.27" };
+            //factory.UserName = "manager";
+            //factory.Password = "ehb";
+            //using (var connection = factory.CreateConnection())
+            //using (var channel = connection.CreateModel())
+            //{
+            //    channel.ExchangeDeclare(exchange: "logs", type: "fanout");
 
-                var message = requests.GetUser(1);
-
-                
-
-                XmlDocument doc = new XmlDocument();
-                doc = (XmlDocument)JsonConvert.DeserializeXmlNode(message, "root");
-
-                //doc.Validate(xsd)
-
-                var body = Encoding.UTF8.GetBytes(doc.InnerXml);
-                channel.BasicPublish(exchange: "logs",
-                                         routingKey: "",
-                                         basicProperties: null,
-                                         body: body);
-                Console.WriteLine(" [x] Sent {0}", doc);
-            }
-
-            Console.WriteLine(" Press [enter] to exit.");
-            Console.ReadLine();
+            //    var message = visReq.GetVisitor(1);
 
 
+
+            //    XmlDocument doc = new XmlDocument();
+            //    doc = (XmlDocument)JsonConvert.DeserializeXmlNode(message, "root");
+
+            //    //doc.Validate();
+
+            //    var body = Encoding.UTF8.GetBytes(doc.InnerXml);
+            //    channel.BasicPublish(exchange: "logs",
+            //                             routingKey: "",
+            //                             basicProperties: null,
+            //                             body: body);
+            //    Console.WriteLine(" [x] Sent {0}", doc);
+            //}
+
+            //Console.WriteLine(" Press [enter] to exit.");
+            //Console.ReadLine();
+
+            var test= visReq.GetVisitor(2);
+
+            Console.WriteLine(test);
+            Console.WriteLine("pause");
 
         }
     }
---
+++ Sender/WrapperTest/Visitors.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sender
+{
+    class Visitors
+    {
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public string email { get; set; }
+        public string gsm { get; set; }
+        public int version { get; set; }
+        public int banned { get; set; }
+        public int isActive { get; set; }
+        public DateTime birthDate { get; set; }
+        public string btwNummer { get; set; }
+        public int gdpr { get; set; }
+
+        public Visitors(string firstname, string lastname, string email, int version)
+        {
+            this.firstname = firstname;
+            this.lastname = lastname;
+            this.email = email;
+            this.version = version;
+
+        }
+    }
+}
